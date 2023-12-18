b8e0778e3beb7fdc0901696f1c876c556494370a
Katulos <katulos@protonmail.com>
2023-12-18 08:35:33 +0000

    Initial commit

---
+++ l10n_ru/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Russia - Accounting",
+    "version": "11.0.0.0.1",
+    "category": "Localization",
+    "author": "CodUP, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-russia",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "data/account_chart.xml",
+        "data/account.account.template.csv",
+        "data/account_chart_template.xml",
+        "data/account_tax_template.xml",
+        "data/account_chart_template.yml",
+    ],
+    "installable": True,
+}
