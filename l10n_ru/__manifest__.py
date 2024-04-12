71a4985a695394a2977782acfa529237be4916c1
katulos <katulos@protonmail.com>
2024-04-12 10:41:38 +0000

    Ро[MIG] l10n_ru: Migration to 14.0

--- l10n_ru/__manifest__.py
+++ l10n_ru/__manifest__.py
@@ -1,17 +1,20 @@
 {
     "name": "Russia - Accounting",
     "version": "14.0.1.0.0",
-    "category": "Localization",
-    "author": "CodUP, Odoo Community Association (OCA)",
+    "category": "Accounting/Localizations/Account Charts",
+    "author": "CodUP, Katulos, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-russia",
     "license": "AGPL-3",
     "depends": ["account"],
     "data": [
-        "data/account_chart.xml",
+        "data/account_account_tag_data.xml",
+        "data/account_chart_template_data.xml",
         "data/account.account.template.csv",
-        "data/account_chart_template.xml",
-        "data/account_tax_template.xml",
-        "data/account_chart_template.yml",
+        "data/account_tax_report_line.xml",
+        "data/account_tax_group_data.xml",  #
+        "data/account_tax_template_data.xml",
+        "data/l10n_ru_chart_post_data.xml",
+        "data/account_fiscal_position_template_data.xml",
+        "data/account_chart_template_configuration_data.xml",
     ],
-    "installable": True,
 }
