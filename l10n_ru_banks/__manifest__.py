e71b3bfb96e0566e3a12d5ad03cbbc705bbbfe4c
Katulos <123748719+Katulos@users.noreply.github.com>
2026-01-08 18:07:50 +0000

    [REF] l10n_ru_banks: The functionality for updating the directory has been implemented

--- l10n_ru_banks/__manifest__.py
+++ l10n_ru_banks/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Russian Banks",
+    "name": "Russian - Banks",
     "summary": "Russian banks and their addresses",
     "version": "17.0.1.0.0",
     "countries": ["ru"],
--- l10n_ru_banks/models/res_bank.py
+++ l10n_ru_banks/models/res_bank.py
@@ -1,12 +1,27 @@
 from odoo import api, fields, models
 
 
+class ResBankCorrAcc(models.Model):
+    _name = "res.bank.corracc"
+    _description = "Correspondent accounts of Russian banks"
+    _rec_name = "corr_acc"
+
+    bank_id = fields.Many2one(comodel_name="res.bank", string="Bank")
+
+    corr_acc = fields.Char(
+        string="Correspondent account",
+        help="Correspondent account used by Russian banks",
+    )
+
+
 class ResBank(models.Model):
     _inherit = "res.bank"
     _description = "Russian Banks"
 
-    corr_acc = fields.Char(
-        "Correspondent account", help="Correspondent account used by Russian banks"
+    corr_acc_ids = fields.One2many(
+        comodel_name="res.bank.corracc",
+        inverse_name="bank_id",
+        string="Correspondent accounts",
     )
 
     country_code = fields.Char(related="country.code", store=False)
@@ -15,12 +30,7 @@ class ResBank(models.Model):
 class ResPartnerBank(models.Model):
     _inherit = "res.partner.bank"
 
-    bank_corr_acc = fields.Char(
-        "Correspondent account", help="Correspondent account used by Russian banks"
-    )
-
     @api.onchange("bank_id")
     def onchange_bank_id(self):
         self.bank_name = self.bank_id.name
         self.bank_bic = self.bank_id.bic
-        self.bank_corr_acc = self.bank_id.corr_acc
